d8e5fc8e5d9e19920a1875d0dec60563df127f38
Nishantd1402 <130464910+Nishantd1402@users.noreply.github.com>
2024-07-02 12:21:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,10 @@
+command = f'pip install -r requirements.txt'
+subprocess.run(command, shell=True)
+
 import streamlit as st
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_community.llms import Ollama
 import os
 import subprocess
 import streamlit.components.v1 as components
