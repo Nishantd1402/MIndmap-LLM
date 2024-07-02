bc88ffe84067b564e048f2ef2c234c80dd298c99
Nishantd1402 <nishantdesale1402@gmail.com>
2024-07-02 14:04:44 +0530

    Changes

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+command = f'pip install -r requirements.txt'
+subprocess.run(command, shell=True)
+
 import streamlit as st
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
