6d328223575b09c16667a3c26b97fb0dc992e737
Nishantd1402 <130464910+Nishantd1402@users.noreply.github.com>
2024-07-02 13:25:43 +0530

    Auto select input from dropdown

--- qna.py
+++ qna.py
@@ -4,6 +4,56 @@ from groq import Groq
 # Initialize the Groq client
 client = Groq(api_key="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")
 
+
+chapter_name = {
+    '1 - Observing the Armies on the Battlefield of Kurukṣetra': 1,
+    '2 - Contents of the Gītā Summarized': 2,
+    '3 - Karma Yoga': 3,
+    '4 - Transcendental Knowledge': 4,
+    '5 - Karma-yoga – Action in Kṛṣṇa Consciousness': 5,
+    '6 - Dhyāna-yoga': 6,
+    '7 - Knowledge of the Absolute': 7,
+    '8 - Attaining the Supreme': 8,
+    '9 - The Most Confidential Knowledge': 9,
+    '10 - Opulence of the Absolute': 10,
+    '11 - The Universal Form': 11,
+    '12 - Devotional Service': 12,
+    '13 - Nature, the Enjoyer and Consciousness': 13,
+    '14 - The Three Modes of Material Nature': 14,
+    '15 - The Yoga of the Supreme Person': 15,
+    '16 - The Divine and Demoniac Natures': 16,
+    '17 - The Divisions of Faith': 17,
+    '18 - Conclusion – The Perfection of Renunciation': 18
+}
+
+
+chapter_verse_dict = {1: ('1', '10', '11', '12', '13', '14', '15', '16-18', '19', '2', '20', '21-22', '23', '24', '25', '26', '27', '28', '29', '3', '30', '31', '32-35', '36', '37-38', '39', '4', '40', '41', '42', '43', '44', '45', '46', '5', '6', '7', '8', '9'), 10: ('1', '10', '11', '12-13', '14', '15', '16', '17', '18', '19', '2', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '3', '30', '31', '32', '33', '34', '35', '36', '37', '38', '39', '4-5', '40', '41', '42', '6', '7', '8', '9'), 11: ('1', '10-11', '12', '13', '14', '15', '16', '17', '18', '19', '2', '20', '21', '22', '23', '24', '25', '26-27', '28', '29', '3', '30', '31', '32', '33', '34', '35', '36', '37', '38', '39', '4', '40', '41-42', '43', '44', '45', '46', '47', '48', '49', '5', '50', '51', '52', '53', '54', '55', '6', '7', '8', '9'), 12: ('1', '10', '11', '12', '13-14', '15', '16', '17', '18-19', '2', '20', '3-4', '5', '6-7', '8', '9'), 13: ('1-2', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '3', '30', '31', '32', '33', '34', '35', '4', '5', '6-7', '8-12'), 14: ('1', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '2', '20', '21', '22-25', '26', '27', '3', '4', '5', '6', '7', '8', '9'), 15: ('1', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '2', '20', '3-4', '5', '6', '7', '8', '9'), 16: ('1-3', '10', '11-12', '13-15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '4', '5', '6', '7', '8', '9'), 17: ('1', '11', '12', '13', '14', '15', '16', '17', '18', '19', '2', '20', '21', '22', '23', '24', '25', '26-27', '28', '3', '4', '5-6', '7', '8-10'), 18: ('1', '10', '11', '12', '13-14', '15', '16', '17', '18', '19', '2', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '3', '30', '31', '32', '33', '34', '35', '36-37', '38', '39', '4', '40', '41', '42', '43', '44', '45', '46', '47', '48', '49', '5', '50', '51-53', '54', '55', '56', '57', '58', '59', '6', '60', '61', '62', '63', '64', '65', '66', '67', '68', '69', '7', '70', '71', '72', '73', '74', '75', '76', '77', '78', '8', '9'), 2: ('1', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '2', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '3', '30', '31', '32', '33', '34', '35', '36', '37', '38', '39', '4', '40', '41', '42-43', '44', '45', '46', '47', '48', '49', '5', '50', '51', '52', '53', '54', '55', '56', '57', '58', '59', '6', '60', '61', '62', '63', '64', '65', '66', '67', '68', '69', '7', '70', '71', '72', '8', '9'), 3: ('1', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '2', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '3', '30', '31', '32', '33', '34', '35', '36', '37', '38', '39', '4', '40', '41', '42', '43', '5', '6', '7', '8', '9'), 4: ('1', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '2', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '3', '30', '31', '32', '33', '34', '35', '36', '37', '38', '39', '4', '40', '41', '42', '5', '6', '7', '8', '9'), 5: ('1', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '2', '20', '21', '22', '23', '24', '25', '26', '27-28', '29', '3', '4', '5', '6', '7', '8-9', 'chapterview'), 6: ('1', '10', '11-12', '13-14', '15', '16', '17', '18', '19', '2', '20-23', '24', '25', '26', '27', '28', '29', '3', '30', '31', '32', '33', '34', '35', '36', '37', '38', '39', '4', '40', '41', '42', '43', '44', '45', '46', '47', '5', '6', '7', '8', '9'), 7: ('1', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '2', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '3', '30', '4', '5', '6', '7', '8', '9'), 8: ('1', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '2', '20', '21', '22', '23', '24', '25', '26', '27', '28', '3', '4', '5', '6', '7', '8', '9'), 9: ('1', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '2', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '3', '30', '31', '32', '33', '34', '4', '5', '6', '7', '8', '9')}
+
+
+
+def extract_purport(file_path):
+    with open(file_path, 'r', encoding='utf-8') as file:
+        lines = file.readlines()
+
+    purport_start = "===== Purport ====="
+    purport_end = "====="
+    
+    purport_lines = []
+    inside_purport = False
+    
+    for line in lines:
+        if purport_start in line:
+            inside_purport = True
+            continue
+        if purport_end in line and inside_purport:
+            inside_purport = False
+            break
+        if inside_purport:
+            purport_lines.append(line.strip('\n'))
+
+    purport_text = '\n'.join(purport_lines)  
+    return purport_text
+
 # Define the system prompt
 system_prompt = """ Generating Questions and Answers from the Purport of a Bhagavad Gita Verse
 
@@ -39,16 +89,20 @@ The characteristics of a truly learned person
 
 Generated Questions and Answers:
 
-What is the significance of the Lord chastising Arjuna, calling him indirectly a fool? 
+What is the significance of the Lord chastising Arjuna, calling him indirectly a fool?
+
 Answer: The Lord is pointing out Arjuna's lack of understanding of the fundamental principles of knowledge, which is essential for making informed decisions and avoiding unnecessary lamentation.
 
-How does one's understanding of the body and soul impact their perspective on life and death? 
+How does one's understanding of the body and soul impact their perspective on life and death?
+
 Answer: Knowing the difference between the body and soul helps one understand that the body is temporary and ephemeral, while the soul is eternal, leading to a shift in focus from material concerns to spiritual growth.
 
-What is the relationship between knowledge and lamentation, according to the Lord? 
+What is the relationship between knowledge and lamentation, according to the Lord?
+
 Answer: The Lord suggests that one who has true knowledge of matter, soul, and the Supreme does not lament, as they understand the temporary nature of the body and the eternal nature of the soul.
 
 Why is knowledge of the Supreme more important than religious formularies, according to the Lord? 
+
 Answer: Knowledge of the Supreme is more important because it provides a deeper understanding of the nature of reality, whereas religious formularies are limited to external practices and rituals.
 
 What are the characteristics of a truly learned person, according to the Lord? 
@@ -75,28 +129,37 @@ Answer: learned
 # Streamlit UI
 st.title("Bhagavad Gita Verse Q&A Generator")
 
-# Input text area
-input_text_area = st.text_area("Input Purport Text")
-
 # Generate button
-if st.button("Generate Questions and Answers"):
-    with st.spinner("Generating..."):
-        # Generate chat completion
-        chat_completion = client.chat.completions.create(
-            messages=[
-                {
-                    "role": "system",
-                    "content": f"{system_prompt}"
-                },
-                {
-                    "role": "user",
-                    "content": f"{input_text_area}",
-                }
-            ],
-            model="llama3-70b-8192",
-            temperature=0.1
-        )
-        output = chat_completion.choices[0].message.content
 
-        # Display output
-        st.markdown(output)
+chapter = st.selectbox("Enter chapter number:", chapter_name.keys(), key=1, index=None, placeholder="Select chapter number...")
+
+if chapter:
+    # Select verses number
+    verses = st.multiselect("Enter verses number:", chapter_verse_dict[chapter_name[chapter]], key=2)
+    chapter = chapter_name[chapter]
+
+    if chapter and verses:
+        for verse in verses:
+            with st.expander(f"Chapter number: {chapter}, Verse: {verse}" , expanded=True):
+                file_path = f"Bhagvada-Gita/{chapter}/{verse}.txt"
+                input_text = extract_purport(file_path)
+                with st.spinner("Generating..."):
+                    # Generate chat completion
+                    chat_completion = client.chat.completions.create(
+                        messages=[
+                            {
+                                "role": "system",
+                                "content": f"{system_prompt}"
+                            },
+                            {
+                                "role": "user",
+                                "content": f"{input_text}",
+                            }
+                        ],
+                        model="llama3-70b-8192",
+                        temperature=0.1
+                    )
+                    output = chat_completion.choices[0].message.content
+
+                    # Display output
+                    st.markdown(output)
