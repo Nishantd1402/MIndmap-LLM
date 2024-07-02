eb0409a1a64a3b39b54899d6a83034db84805755
Nishantd1402 <130464910+Nishantd1402@users.noreply.github.com>
2024-07-02 12:18:15 +0530

    cleaning app.py

--- app.py
+++ app.py
@@ -1,7 +1,4 @@
 import streamlit as st
-from langchain_core.prompts import ChatPromptTemplate
-from langchain_core.output_parsers import StrOutputParser
-from langchain_community.llms import Ollama
 import os
 import subprocess
 import streamlit.components.v1 as components
